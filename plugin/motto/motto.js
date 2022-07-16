957743f7fb54acc1c0f2cae4c55625119289f6d2
Yxxxb <2947934317@qq.com>
2022-07-16 22:02:41 +0800

    Site updated: 2022-07-16 22:02:40

---
+++ plugin/motto/motto.js
@@ -0,0 +1,44 @@
+/**
+  随机获取名言.
+
+  author:prd.
+  version:2016.7.22
+  note:名言均来自[一个](https://www.wufazhuce.com)
+
+  使用示例(关于使用的调用,请参考最底部的函数.):
+        1.先引入本js.
+        2.在想使用的地方,调用getMingYan()函数即可获取名言.[getMingYanHref()获取可以跳转至One的名言,getMingYanContent():只获取名言内容]
+        3.名言中存储为json数据,index对应One的每一期号,content是名言.
+        4.请尊重版权,在展示名言的同时,可将名言链接至One.例如:<a href="https://wufazhuce.com/one/14">是狼是人，日久见心。</br> - </br>小饭</a>其中的14为期号.
+*/
+var mingyan=new Array();
+mingyan.push("{'index':0,'content':'Welcome to my blog @ Yxxxb & Xubing Ye</br> - </br>Live a good life meet slowly.'}");
+
+/**
+    随机获取名言.
+
+    直接调用此函数即可.
+*/
+function getMingYan(){
+  return (mingyan[0]);
+}
+
+/*
+  获取名言,并链接至One.
+*/
+function getMingYanHref(){
+  var item = getItem();
+  return "<a href='https://wufazhuce.com/one/"+item.index+"' target='_blank'>"+item.content+"</a>";
+}
+
+function getItem(){
+  return eval('(' +(mingyan[0])+ ')');
+}
+
+/*
+  只获取内容.
+*/
+function getMingYanContent(){
+  var item = getItem();
+  return item['content'];
+}
